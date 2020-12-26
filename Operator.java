6cfddf385a64959ef318d2a981011f69e76baad8
Kareemelagroudy <58118555+Kareemelagroudy@users.noreply.github.com>
2020-12-26 22:27:52 +0200

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.operator;
+
+/**
+ *
+ * @author kokye
+ */
+public class Operator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public int sub(int a, int b)
+    {
+        return a-b;
+    }
+    public int divide(int a, int b)
+    {
+        return a/b;
+    }
+    public static void main(String args[]) {
+        // TODO code application logic here
+    }
+}
